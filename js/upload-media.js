6a5a58ef811b0ab7dde2ed7101f0a9be5562596e
ALRICPAON <paonalric@gmail.com>
2025-06-12 21:43:41 +0200

    Update and rename upload-image.js to upload-media.js

--- js/upload-media.js
+++ js/upload-media.js
@@ -17,13 +17,37 @@ export async function uploadCoverImage(file, tunnelName) {
 
   try {
     const uploadResult = await uploadBytes(storageRef, file);
-    console.log("✅ Upload réussi :", uploadResult);
+    console.log("✅ Upload image réussi :", uploadResult);
 
     const url = await getDownloadURL(storageRef);
     console.log("✅ URL de l'image :", url);
     return url;
   } catch (error) {
-    console.error("❌ Erreur d'upload :", error);
+    console.error("❌ Erreur d'upload image :", error);
+    throw error;
+  }
+}
+
+export async function uploadCustomVideo(file, tunnelName) {
+  const user = auth.currentUser;
+  if (!user) throw new Error("Utilisateur non connecté");
+
+  console.log("Début upload vidéo...");
+  console.log("Nom du fichier :", file.name);
+  console.log("Nom du tunnel :", tunnelName);
+
+  const storagePath = `tunnels/${user.uid}/${tunnelName}/video.mp4`;
+  const storageRef = ref(storage, storagePath);
+
+  try {
+    const uploadResult = await uploadBytes(storageRef, file);
+    console.log("🎥 Upload vidéo réussi :", uploadResult);
+
+    const url = await getDownloadURL(storageRef);
+    console.log("🎥 URL de la vidéo :", url);
+    return url;
+  } catch (error) {
+    console.error("❌ Erreur d'upload vidéo :", error);
     throw error;
   }
 }
