a2586d7db374944467ccc4b319e535c074420194
ALRICPAON <paonalric@gmail.com>
2025-06-13 09:29:31 +0200

    Create firebase-init.js

---
+++ js/firebase-init.js
@@ -0,0 +1,13 @@
+// firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2yzKA3kESPjgcFk6pojJQK4rNToywqJI",
+  authDomain: "sellyo-3bbdb.firebaseapp.com",
+  projectId: "sellyo-3bbdb",
+  storageBucket: "sellyo-3bbdb.appspot.com",
+  messagingSenderId: "465249279278",
+  appId: "1:465249279278:web:319844f7477ab47930eebf"
+};
+
+export const app = initializeApp(firebaseConfig);
