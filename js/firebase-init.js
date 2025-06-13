75e2de26e4b022ea4308abd2e905d09f0e0bd6d5
ALRICPAON <paonalric@gmail.com>
2025-06-13 15:36:30 +0200

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyC2yzKA3kESPjgcFk6pojJQK4rNToywqJI",
   authDomain: "sellyo-3bbdb.firebaseapp.com",
   projectId: "sellyo-3bbdb",
-  storageBucket: "sellyo-3bbdb.appspot.com",
+  storageBucket: "sellyo-3bbdb.firebasestorage.app", // ✅ version correcte
   messagingSenderId: "465249279278",
   appId: "1:465249279278:web:319844f7477ab47930eebf"
 };
